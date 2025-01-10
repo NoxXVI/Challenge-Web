764ff09a5fffde250997dde6a04b32fb01712d69
bouss <inivasim4@gmail.com>
2025-01-10 16:57:50 +0100

    emailjs

--- form.js
+++ form.js
@@ -1,6 +1,14 @@
 "use strict"
 
+import emailjs from "emailjs";
+
 const bouton = document.createElement('button');
 bouton.addEventListener(click, () => {
-    
+    emailjs.sendForm('service_r255z1x', 'template_fusgldc', form)
+    .then((response) => {
+        console.log('Email envoyé avec succès !', response.status, response.text);
+    })
+    .catch((error) => {
+        console.error('Erreur lors de l\'envoi de l\'email :', error);
+    });
 })
