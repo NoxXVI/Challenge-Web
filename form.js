d2ddccfef393fb6f3256907bafa04f66ada3225a
Timothé Leroy <77456834+NoxXVI@users.noreply.github.com>
2025-01-14 08:25:15 +0100

    Initial Commit

--- form.js
+++ form.js
@@ -1,9 +1,8 @@
-"use strict"
+// import emailjs from "./node_modules/emailjs";
 
-import emailjs from "emailjs";
-
-const bouton = document.createElement('button');
-bouton.addEventListener(click, () => {
+const bouton = document.getElementById('bouton');
+console.log(bouton);
+bouton.addEventListener(SubmitEvent, () => {
     emailjs.sendForm('service_r255z1x', 'template_fusgldc', form)
     .then((response) => {
         console.log('Email envoyé avec succès !', response.status, response.text);
@@ -11,4 +10,7 @@ bouton.addEventListener(click, () => {
     .catch((error) => {
         console.error('Erreur lors de l\'envoi de l\'email :', error);
     });
+    bouton.preventDefault()
 })
+
+
